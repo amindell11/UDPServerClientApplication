611bb9255d28568850238ec263aa8d404c2f2a98
Ari Mindell <amindell11@gmail.com>
2016-06-01 12:42:48 -0400

    formatting

--- UDP Server Client/src/Client.java
+++ UDP Server Client/src/Client.java
@@ -11,78 +11,92 @@ import net.communication.SimpleExchangeComm;
 import net.communication.SimpleExchangeComm.simpleExchange.simpleExchangeRequest;
 import net.communication.SimpleExchangeComm.simpleExchange.simpleExchangeRequest.RequestType;
 
-
 public class Client {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		// Find the server using UDP broadcast
 		try {
-		  //Open a random port to send the package
-		  DatagramSocket c = new DatagramSocket();
-		  c.setBroadcast(true);
+			// Open a random port to send the package
+			DatagramSocket c = new DatagramSocket();
+			c.setBroadcast(true);
 
-		  byte[] sendData = "DISCOVER_FUIFSERVER_REQUEST".getBytes();
+			byte[] sendData = "DISCOVER_FUIFSERVER_REQUEST".getBytes();
 
-		  //Try the 255.255.255.255 first
-		  try {
-		    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName("255.255.255.255"), 8888);
-		    c.send(sendPacket);
-		    System.out.println(">>> Request packet sent to: 255.255.255.255 (DEFAULT)");
-		  } catch (Exception e) {
-		  }
+			// Try the 255.255.255.255 first
+			try {
+				DatagramPacket sendPacket = new DatagramPacket(sendData,
+						sendData.length,
+						InetAddress.getByName("255.255.255.255"), 8888);
+				c.send(sendPacket);
+				System.out
+						.println(">>> Request packet sent to: 255.255.255.255 (DEFAULT)");
+			} catch (Exception e) {
+			}
 
-		  // Broadcast the message over all the network interfaces
-		  Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-		  while (interfaces.hasMoreElements()) {
-		    NetworkInterface networkInterface = interfaces.nextElement();
+			// Broadcast the message over all the network interfaces
+			Enumeration<NetworkInterface> interfaces = NetworkInterface
+					.getNetworkInterfaces();
+			while (interfaces.hasMoreElements()) {
+				NetworkInterface networkInterface = interfaces.nextElement();
 
-		    if (networkInterface.isLoopback() || !networkInterface.isUp()) {
-		      continue; // Don't want to broadcast to the loopback interface
-		    }
+				if (networkInterface.isLoopback() || !networkInterface.isUp()) {
+					continue; // Don't want to broadcast to the loopback
+								// interface
+				}
 
-		    for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) {
-		      InetAddress broadcast = interfaceAddress.getBroadcast();
-		      if (broadcast == null) {
-		        continue;
-		      }
+				for (InterfaceAddress interfaceAddress : networkInterface
+						.getInterfaceAddresses()) {
+					InetAddress broadcast = interfaceAddress.getBroadcast();
+					if (broadcast == null) {
+						continue;
+					}
 
-		      // Send the broadcast package!
-		      try {
-		        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, broadcast, 8888);
-		        c.send(sendPacket);
-		      } catch (Exception e) {
-		      }
+					// Send the broadcast package!
+					try {
+						DatagramPacket sendPacket = new DatagramPacket(
+								sendData, sendData.length, broadcast, 8888);
+						c.send(sendPacket);
+					} catch (Exception e) {
+					}
 
-		      System.out.println(">>> Request packet sent to: " + broadcast.getHostAddress() + "; Interface: " + networkInterface.getDisplayName());
-		    }
-		  }
+					System.out.println(">>> Request packet sent to: "
+							+ broadcast.getHostAddress() + "; Interface: "
+							+ networkInterface.getDisplayName());
+				}
+			}
 
-		  System.out.println(">>> Done looping over all network interfaces. Now waiting for a reply!");
+			System.out
+					.println(">>> Done looping over all network interfaces. Now waiting for a reply!");
 
-		  //Wait for a response
-		  byte[] recvBuf = new byte[15000];
-		  DatagramPacket receivePacket = new DatagramPacket(recvBuf, recvBuf.length);
-		  c.receive(receivePacket);
+			// Wait for a response
+			byte[] recvBuf = new byte[15000];
+			DatagramPacket receivePacket = new DatagramPacket(recvBuf,
+					recvBuf.length);
+			c.receive(receivePacket);
 
-		  //We have a response
-		  System.out.println(">>> Broadcast response from server: " + receivePacket.getAddress().getHostAddress());
+			// We have a response
+			System.out.println(">>> Broadcast response from server: "
+					+ receivePacket.getAddress().getHostAddress());
 
-		  //Check if the message is correct
-		  String message = new String(receivePacket.getData()).trim();
-		  if (message.equals("DISCOVER_FUIFSERVER_RESPONSE")) {
-		    //DO SOMETHING WITH THE SERVER'S IP (for example, store it in your controller)
-			  SimpleExchangeComm.simpleExchange.Builder builder = SimpleExchangeComm.simpleExchange.newBuilder();
-			  
-			  builder.setRequest(simpleExchangeRequest.newBuilder().setRequestType(RequestType.SERVER_INFO));
-			  ByteArrayOutputStream aOutput = new ByteArrayOutputStream(15000);
-			  builder.build().writeDelimitedTo(aOutput);
-			  byte[] req=aOutput.toByteArray();
-			  DatagramPacket sendPacket = new DatagramPacket(req, req.length, receivePacket.getAddress(), 8888);
-			  System.out.println(sendPacket.getData());
-			  c.send(sendPacket);
-		  }
+			// Check if the message is correct
+			String message = new String(receivePacket.getData()).trim();
+			if (message.equals("DISCOVER_FUIFSERVER_RESPONSE")) {
+				// DO SOMETHING WITH THE SERVER'S IP (for example, store it in
+				// your controller)
+				SimpleExchangeComm.simpleExchange.Builder builder = SimpleExchangeComm.simpleExchange
+						.newBuilder();
 
-		  //Close the port!
-		  c.close();
+				builder.setRequest(simpleExchangeRequest.newBuilder()
+						.setRequestType(RequestType.SERVER_INFO));
+				ByteArrayOutputStream aOutput = new ByteArrayOutputStream(15000);
+				builder.build().writeDelimitedTo(aOutput);
+				byte[] req = aOutput.toByteArray();
+				DatagramPacket sendPacket = new DatagramPacket(req, req.length,
+						receivePacket.getAddress(), 8888);
+				c.send(sendPacket);
+			}
+
+			// Close the port!
+			c.close();
 		} catch (IOException ex) {
 		}
 	}
