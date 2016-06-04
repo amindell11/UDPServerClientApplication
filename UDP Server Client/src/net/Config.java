38b4d6406131f9391d3117e1298908cceb2013a7
ari <amindell11@gmail.com>
2016-06-04 17:49:30 -0500

    added request membership method
    TODO write server membership review method

--- UDP Server Client/src/net/Config.java
+++ UDP Server Client/src/net/Config.java
@@ -3,4 +3,6 @@ package net;
 public class Config {
 	public static final int PORT=8888;
 	public static final int MAX_CLIENTS=16;
+	public static final int DEFAULT_TIMEOUT=1000;
+	public static final boolean REQUIRE_UNIQUE_CLIENTS=false;
 }
--- UDP Server Client/src/net/connectionutil/ClientConnectionUtil.java
+++ UDP Server Client/src/net/connectionutil/ClientConnectionUtil.java
@@ -1,10 +1,27 @@
 package net.connectionutil;
 
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.io.IOException;
+import java.net.DatagramPacket;
 
-import net.server.ServerInfo;
+import net.Config;
+import net.SimpleExchangePacket;
+import net.communication.SimpleExchangeComm.simpleExchange.simpleExchangeRequest.RequestType;
+import net.communication.SimpleExchangeComm.simpleExchange.simpleExchangeResponse;
+import net.communication.SimpleExchangeComm.simpleExchange.simpleExchangeResponse.ResponseType;
 
-public class ClientConnectionUtil extends ConnectionUtil{
-
+public class ClientConnectionUtil extends ConnectionUtil {
+	public String requestClusterMembership(String serverAddress, int port, String username) throws IOException {
+		getUtilSocket().send(new SimpleExchangePacket(RequestType.CLUSTER_MEMBERSHIP_REQUEST, username)
+				.getPacket(serverAddress, port));
+		DatagramPacket responsePacket = receivePacket(Config.DEFAULT_TIMEOUT);
+		if(responsePacket==null)return "Packet timed out. Server unavailable";
+		simpleExchangeResponse response = new SimpleExchangePacket(responsePacket.getData()).getResponse();
+		if(response.getResponseType().equals(ResponseType.CLUSTER_MEMBERSHIP_ACCEPT)){
+			return null;
+		}else if(response.getResponseType().equals(ResponseType.CLUSTER_MEMBERSHIP_DENIED)){
+			return response.getResponseNote();
+		}else{
+			return "Unknown error.";
+		}
+	}
 }
