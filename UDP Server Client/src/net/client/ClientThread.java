9b34fac3ff380141df2d9c7cdb6d63541efbe0e0
Ari <amindell11@gmail.com>
2016-06-05 22:19:24 -0400

    started to add update code for client side

--- UDP Server Client/src/net/client/ClientThread.java
+++ UDP Server Client/src/net/client/ClientThread.java
@@ -19,16 +19,41 @@ public class ClientThread extends Thread {
 	int id;
 	String address;
 	private boolean activeMember;
+	private boolean open;
 
 	public ClientThread(String username, String address) {
 		this.username = username;
 		activeMember = false;
+		open = false;
 		this.address = address;
 	}
 
+	@Override
+	public void run() {
+		if (!activeMember) {
+			System.out.println("Error: Client not connected to a server. Cannot begin update cycle.");
+			return;
+		}
+		open = true;
+		while (open) {
+			try {
+				update();
+			} catch (IOException e) {
+				System.out.println("update tripped. attempting to resume.");
+			}
+		}
+	}
+
+	public void update() throws IOException {
+		DatagramPacket receive=ConnectionUtil.receivePacket();
+		handlePacket(receive);
+	}
+	public void handlePacket(DatagramPacket packet){
+		//TODO
+	}
 	public String requestClusterMembership(String serverAddress, int port) throws IOException {
-		ConnectionUtil.getUtilSocket().send(new SimpleExchangePacket(RequestType.CLUSTER_MEMBERSHIP_REQUEST, username).getPacket(serverAddress,
-				port));
+		ConnectionUtil.getUtilSocket().send(new SimpleExchangePacket(RequestType.CLUSTER_MEMBERSHIP_REQUEST, username)
+				.getPacket(serverAddress, port));
 		DatagramPacket responsePacket = ConnectionUtil.receivePacket(Config.DEFAULT_TIMEOUT);
 		if (responsePacket == null) {
 			return "Packet timed out. Server unavailable";
@@ -38,14 +63,15 @@ public class ClientThread extends Thread {
 		if (response.getResponseType().equals(ResponseType.CLUSTER_MEMBERSHIP_ACCEPT)) {
 			int id = responseMsg.getId();
 			this.id = id;
-			activeMember=true;
+			activeMember = true;
 			return null;
 		} else if (response.getResponseType().equals(ResponseType.CLUSTER_MEMBERSHIP_DENIED)) {
 			return response.getResponseNote();
 		}
 		return "Unknown error.";
 	}
-	public boolean isActiveMember(){
+
+	public boolean isActiveMember() {
 		return activeMember;
 	}
 }
