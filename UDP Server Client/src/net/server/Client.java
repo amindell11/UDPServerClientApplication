e7d951c0fabdf7e3a4f1599479fdd3e6924a1f20
Josh <joshuaengels@yahoo.com>
2016-06-06 20:59:50 -0400

    I fixed the ping method to ensure client threads kill themselves on time

--- UDP Server Client/src/net/server/Client.java
+++ UDP Server Client/src/net/server/Client.java
@@ -11,76 +11,82 @@ import net.communication.SimpleExchangeComm.simpleExchange.simpleExchangeRequest
 import net.connectionutil.ConnectionUtil;
 
 public class Client extends Thread{
-	public static List<Integer> usedIds;
-	private String username;
-	private int id;
-	private String address;
-	private long lastCommTimestamp;
-	private boolean open;
-	private ServerThread parent;
+    public static List<Integer> usedIds;
+    private String username;
+    private int id;
+    private String address;
+    private long lastCommTimestamp;
+    private boolean open;
+    private ServerThread parent;
+    private long lastSentPingTimestamp;
 
-	public Client(String username, int id, String address, ServerThread parent){
-		this.username = username;
-		this.id = id;
-		this.address = address;
-		this.parent = parent;
-		open = true;
-		lastCommTimestamp = System.currentTimeMillis();
+    public Client(String username, int id, String address, ServerThread parent){
+	this.username = username;
+	this.id = id;
+	this.address = address;
+	this.parent = parent;
+	open = true;
+	lastCommTimestamp = System.currentTimeMillis();
+	lastSentPingTimestamp = 0;
+    }
+
+    public void handleMessage(simpleExchange msg){
+	lastCommTimestamp = System.currentTimeMillis();
+    }
+
+    public static int assignNewId(){
+	if(usedIds == null){
+	    usedIds = new ArrayList<>();
+	}
+	int generatedId = 1;
+	while(usedIds.contains(generatedId)){
+	    generatedId++;
+	}
+	usedIds.add(generatedId); 
+	return generatedId;
+    }
+
+    @Override
+    public void run(){
+	while(open){
+	    open=isClientStillActive();
+	}
+	System.out.println("Client no longer active. Closing.");
+    }
+    public boolean isClientStillActive(){
+	long currentTime = System.currentTimeMillis();
+	long timeSinceLastComm = lastCommTimestamp - currentTime;
+
+	//If this is true, kill the thread by returning false
+	//The thread has been unresponsive for more than 20 seconds
+	if(timeSinceLastComm > 20000){
+	    return false;
 	}
 
-	public void handleMessage(simpleExchange msg){
-		System.out.println("gaht it");
-		lastCommTimestamp = System.currentTimeMillis();
+	//Ping the client if the time since last communication is more than 10 seconds
+	//Also don't ping again if we already sent a ping less than a second ago
+	else if(timeSinceLastComm > 10000 && lastSentPingTimestamp - currentTime > 1000){
+	    lastSentPingTimestamp = currentTime;
+	    System.out.println("New Ping");
+	    try{
+		parent.socket.send(new SimpleExchangePacket(RequestType.CLIENT_PING, "").getPacket(getAddress(), parent.info.getPort()));
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    }
 	}
+	return true;
 
-	public static int assignNewId(){
-		if(usedIds == null){
-			usedIds = new ArrayList<>();
-		}
-		int generatedId = 1;
-		while(usedIds.contains(generatedId)){
-			generatedId++;
-		}
-		usedIds.add(generatedId); 
-		return generatedId;
-	}
-
-	@Override
-	public void run(){
-		while(open){
-			open=isClientStillActive();
-		}
-		System.out.println("Client no longer active. Closing.");
-	}
-	public boolean isClientStillActive(){
-		long currentTime = System.currentTimeMillis();
-		long timeSinceCommed = currentTime-lastCommTimestamp;
-		if(timeSinceCommed > Config.CLIENT_MAX_DEAD_TIME){
-				try{
-					System.out.println("client inactive. pinging.");
-					parent.socket.send(new SimpleExchangePacket(RequestType.CLIENT_PING, "").getPacket(getAddress(), parent.info.getPort()));
-					ConnectionUtil.receivePacket(Config.DEFAULT_TIMEOUT);
-					System.out.println("finished waiting");
-					lastCommTimestamp=currentTime;
-					return true;
-				} catch (IOException e) {
-					System.out.println("ping was not returned. Concluding client must be closed.");
-					return false;
-				}
-		}
-		return true;
-
-	}
-	public void close() {
-		open = false;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public int getClientId(){
-		return id;
-	}
+    }
+    public void close() {
+	open = false;
+    }
+    public String getAddress() {
+	return address;
+    }
+    public String getUsername() {
+	return username;
+    }
+    public int getClientId(){
+	return id;
+    }
 }
