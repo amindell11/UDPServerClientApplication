6bfb66a7e86f624d09149e163ed7bd772d62de20
Ari Mindell <amindell11@gmail.com>
2016-06-09 14:07:35 -0400

    removed printlns

--- OnlineBoxGame/src/game/GameManager.java
+++ OnlineBoxGame/src/game/GameManager.java
@@ -1,4 +1,5 @@
 package game;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,43 +9,41 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
 import game_object.GameObject;
+
 /**
  * @author Ari
  */
-public class GameManager extends BasicGame
-{
+public class GameManager extends BasicGame {
 	List<GameObject> objects;
 	String username;
-    public GameManager()
-    {
-        super("Box game");
-        this.username="";
-    	objects=new ArrayList<>();
-    }
 
-    @Override
-    public void init(GameContainer container) throws SlickException
-    {
-    }
- 
-    @Override
-    public void update(GameContainer container, int delta) throws SlickException
-    {
-    	System.out.println("start using list");
-    	synchronized(objects){
-    	for(GameObject object:objects){
-    		object.update(container, delta);
-    	}
-    	System.out.println("finished using list");
-    	}
-    	
-    }
- 
-    public void render(GameContainer container, Graphics g) throws SlickException
-    {
-    	g.drawString(username, 10, 30);
-    }
-    public void setUsername(String username){
-    	this.username=username;
-    }
+	public GameManager() {
+		super("Box game");
+		this.username = "";
+		objects = new ArrayList<>();
+	}
+
+	@Override
+	public void init(GameContainer container) throws SlickException {
+	}
+
+	@Override
+	public void update(GameContainer container, int delta)
+			throws SlickException {
+		synchronized (objects) {
+			for (GameObject object : objects) {
+				object.update(container, delta);
+			}
+		}
+
+	}
+
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		g.drawString(username, 10, 30);
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 }
