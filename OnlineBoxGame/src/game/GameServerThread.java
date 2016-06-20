a4a949cbd49853e269e1d1250e8948c785ecbfd1
amindell11 <amindell11@gmail.com>
2016-06-20 16:41:19 -0400

    added newObject message handling code to game server

--- OnlineBoxGame/src/game/GameServerThread.java
+++ OnlineBoxGame/src/game/GameServerThread.java
@@ -1,8 +1,13 @@
 package game;
 
+import java.io.IOException;
+
 import hooks.UnhandledMessageHook;
 import net.proto.ExchangeProto.Exchange;
 import net.server.ServerThread;
+import proto.GameStateExchangeProto.GameStateExchange;
+import proto.GameStateExchangeProto.GameStateExchange.ObjectCreatedNotice;
+import proto.GameStateExchangeProto.GameStateExchange.StateExchangeType;
 
 /**
  * This class manages the game update messages sent from clients. It recieves
@@ -45,7 +50,26 @@ public class GameServerThread extends Thread implements UnhandledMessageHook {
 
 	@Override
 	public void handleMessage(Exchange message) {
-
+		if (message.hasExtension(GameStateExchange.gameUpdate)) {
+			GameStateExchange update = message.getExtension(GameStateExchange.gameUpdate);
+			switch (update.getPurpose()) {
+			case NEW_OBJECT:
+				ObjectCreatedNotice ex = ObjectCreatedNotice.newBuilder(update.getNewObject()).setObjectId(0).build();
+				try {
+					server.announceToClients(Exchange.newBuilder().setExtension(GameStateExchange.gameUpdate,
+							GameStateExchange.newBuilder().setNewObject(ex).setPurpose(StateExchangeType.NEW_OBJECT).build()).build());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				break;
+			case OBJECT_UPDATE:
+				break;
+			case STALE_OBJECT:
+				break;
+			default:
+				break;
+			}
+		}
 	}
 
 }
